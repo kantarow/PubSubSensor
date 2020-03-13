da1b08385cbcd6a17a4b0518e208dc2819446f67
kantarow <kanta7306@gmail.com>
2020-03-13 23:51:36 +0900

    paho-mqttのsubscribeのexampleを追加 * コメントを一部日本語に修正 * subscribeするトピックを変更 * mosquitto_pubを利用して動作を確認する方法をコメントで記載

---
+++ examples/paho_example.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+
+# クライアントがサーバーからCONNACKを受けとった際に呼ばれるコールバック
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # on_connect()にsubscribe()を記述すると, 接続が切断されても再び購読できる
+    client.subscribe("example/topic")
+
+# クライアントがサーバーからPUBLISHを受け取った際に呼ばれるコールバック
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mqtt.eclipse.org", 1883, 60)
+
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+
+# mosquitto_pubを利用してこのサンプルコードでメッセージを受け取る
+# mosquitto_pub -h mqtt.eclipse.org -p 1883 -t "example/topic" -m "hello"
+
+client.loop_forever()
