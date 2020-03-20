737ce3c5f78cb284796f043b3aea8bcfce7b4144
nanigasi_san <nanigasi.py@gmail.com>
2020-03-21 01:39:22 +0900

    クラス名の変更とメンバの属性の変更
    抽象基底クラスと一部具象サブクラスの命名をした。
    一部のメンバをプライベートにした

--- i2c_mp.py
+++ i2c_mp.py
@@ -1,17 +1,17 @@
 from multiprocessing import Process, Manager
-from time import sleep
+from time import sleep, time
 from abc import ABCMeta, abstractmethod
 
 
-class BaseClass(metaclass=ABCMeta):
+class I2CBase(metaclass=ABCMeta):
     def __init__(self):
-        self._m = Manager()
-        self.status_dict = self._m.dict({})
+        self.__m = Manager()
+        self.status_dict = self.__m.dict({})
 
         self.setup()
 
-        self._p = Process(target=self.process, args=())
-        self._p.start()
+        self.__p = Process(target=self.process, args=())
+        self.__p.start()
 
     @abstractmethod
     def setup(self):
@@ -22,39 +22,48 @@ class BaseClass(metaclass=ABCMeta):
         pass
 
 
-class Aclass(BaseClass):
+class Thermistor(I2CBase):
     def __init__(self):
         super().__init__()
 
     def setup(self):
-        self.status_dict["type"] = "sensor_a"
-        self.status_dict["model_number"] = "hogehoge"
+        self.status_dict["type"] = "thermistor"
+        self.status_dict["model_number"] = "103JT-050"
         self.status_dict["measured_time"] = 0
+        self.status_dict["temperature_celsius"] = 0
 
     def process(self):
         while True:
             sleep(1)
-            self.status_dict["measured_time"] += 1
+            self.status_dict["temperature_celsius"] += 1
+            self.status_dict["measured_time"] = time()
 
 
-class Bclass(BaseClass):
+class PressureSensor(I2CBase):
     def __init__(self):
         super().__init__()
 
     def setup(self):
-        self.status_dict["type"] = "sensor_b"
-        self.status_dict["model_number"] = "hugahuga"
+        self.status_dict["type"] = "pressure_sensor"
+        self.status_dict["model_number"] = "LPS251B"
         self.status_dict["measured_time"] = 0
+        self.status_dict["pressure_hpa"] = 0
+        self.status_dict["temperature_celsius"] = 0
+        self.status_dict["altitude_meters"] = 0
 
     def process(self):
         while True:
             sleep(1.5)
-            self.status_dict["measured_time"] += 1
+            self.status_dict["measured_time"] = time()
+            self.status_dict["pressure_hpa"] += 1
+            self.status_dict["temperature_celsius"] += 1
+            self.status_dict["altitude_meters"] += 1
 
 
 if __name__ == "__main__":
-    A = Aclass()
-    B = Bclass()
+    A = Thermistor()
+    B = PressureSensor()
     while True:
         sleep(2)
-        print(A.status_dict.copy(), B.status_dict.copy())
+        print(A.status_dict.copy())
+        print(B.status_dict.copy())
