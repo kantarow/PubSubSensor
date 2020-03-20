313ebb1a1f38f2a74aa37be703b36d75dde23faa
nanigasi_san <nanigasi.py@gmail.com>
2020-03-21 07:30:38 +0900

    圧力センサーのクラスを実装

--- i2c_mp.py
+++ i2c_mp.py
@@ -1,12 +1,12 @@
 from multiprocessing import Process, Value
 from multiprocessing.sharedctypes import Synchronized
-from time import sleep
+from time import sleep, time
 from abc import ABCMeta, abstractmethod
 
 
 class I2CSensorBase(metaclass=ABCMeta):
     """
-    Base class of I2C Sensor.
+    I2Cセンサーを表す基底クラス
     """
 
     def __init__(self, bus, address):
@@ -22,6 +22,7 @@ class I2CSensorBase(metaclass=ABCMeta):
 
     @abstractmethod
     def process(self):
+        # TODO: Lockを使うようにする
         pass
 
     @property
@@ -36,12 +37,11 @@ class I2CSensorBase(metaclass=ABCMeta):
 
 
 class Thermistor(I2CSensorBase):
-
     def __init__(self, bus, address):
         self.type = "thermistor"
         self.model_number = "103JT-050"
-        self.measured_time = Value("i", 0)
-        self.temperature_celsius = Value("i", 0)
+        self.measured_time = Value("d", 0.0)
+        self.temperature_celsius = Value("d", 0.0)
         super().__init__(bus, address)
 
     def setup(self):
@@ -49,19 +49,41 @@ class Thermistor(I2CSensorBase):
 
     def process(self):
         while True:
-            sleep(2)
-            self.measured_time.value += 1
+            sleep(4)
+            self.measured_time.value = time()
             self.temperature_celsius.value += 1
 
 
-# TODO: 圧力センサークラス
+class PressureSensor(I2CSensorBase):
+    def __init__(self, bus, address):
+        self.type = "pressure_sensor"
+        self.model_number = "LPS251B"
+        self.measured_time = Value("d", 0.0)
+        self.pressure_hpa = Value("d", 0.0)
+        self.temperature_celsius = Value("d", 0.0)
+        self.altitude_meters = Value("d", 0.0)
+        super().__init__(bus, address)
+
+    def setup(self):
+        pass
+
+    def process(self):
+        while True:
+            sleep(2.5)
+            self.measured_time.value = time()
+            self.pressure_hpa.value += 0.1
+            self.temperature_celsius.value += 0.1
+            self.altitude_meters.value += 0.1
+
 # TODO: 加速度センサークラス
 # TODO: 温湿度センサークラス
 # TODO: 脈波センサークラス
 
+
 if __name__ == "__main__":
     Th1 = Thermistor("bus", 0x40)
     Th2 = Thermistor("bus", 0x60)
+    Pr = PressureSensor("bus", 0x80)
     while True:
-        sleep(2)
-        print(Th1.status_dict, Th2.status_dict)
+        sleep(1)
+        print(Th1.status_dict, Th2.status_dict, Pr.status_dict)
