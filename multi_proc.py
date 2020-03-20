0c6e56055e64462b6dc9332aec749d18e6670d32
nanigasi_san <nanigasi.py@gmail.com>
2020-03-20 23:45:29 +0900

    抽象基底クラスを定義

--- multi_proc.py
+++ multi_proc.py
@@ -1,13 +1,13 @@
 from multiprocessing import Process, Manager
 from time import sleep
+from abc import ABCMeta, abstractmethod
 
-# TODO: 各クラスで__processを実行するように変更(白久さんのを参考に)
 # TODO: 各センサの抽象基底クラスをabcで実装(__setupと__processだけ定義するようにする)
 # TODO: 各センサの値は読み込み完了次第辞書として更新する
 # TODO: ↑を総括して一つの辞書を返すクラスをつくる
 
 
-class Aclass:
+class BaseClass(metaclass=ABCMeta):
     def __init__(self):
         self._m = Manager()
         self.status_dict = self._m.dict({})
@@ -16,6 +16,19 @@ class Aclass:
 
         self._p = Process(target=self.process, args=())
         self._p.start()
+
+    @abstractmethod
+    def setup(self):
+        pass
+
+    @abstractmethod
+    def process(self):
+        pass
+
+
+class Aclass(BaseClass):
+    def __init__(self):
+        super().__init__()
 
     def setup(self):
         self.status_dict["type"] = "sensor_a"
@@ -28,15 +41,9 @@ class Aclass:
             self.status_dict["measured_time"] += 1
 
 
-class Bclass:
+class Bclass(BaseClass):
     def __init__(self):
-        self._m = Manager()
-        self.status_dict = self._m.dict({})
-
-        self.setup()
-
-        self._p = Process(target=self.process, args=())
-        self._p.start()
+        super().__init__()
 
     def setup(self):
         self.status_dict["type"] = "sensor_b"
@@ -52,6 +59,6 @@ class Bclass:
 if __name__ == "__main__":
     A = Aclass()
     B = Bclass()
-    for i in range(10):
-        sleep(3)
+    while True:
+        sleep(2)
         print(A.status_dict.copy(), B.status_dict.copy())
