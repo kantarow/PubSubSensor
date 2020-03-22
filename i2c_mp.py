db68c644263bb3e71fab9f0f738a93949b086a2a
nanigasi_san <nanigasi.py@gmail.com>
2020-03-23 02:51:18 +0900

    基底クラスにdocstringを追加

--- i2c_mp.py
+++ i2c_mp.py
@@ -7,10 +7,32 @@ from abc import ABCMeta, abstractmethod
 # TODO: 例外処理
 class I2CSensorBase(metaclass=ABCMeta):
     """
-    I2Cセンサーを表す基底クラス
+    I2Cセンサーを表す抽象基底クラス
+
+    Attributes
+    ----------
+    _bus : smbus2.SMBus
+        i2cのバス
+    _address : int
+        そのセンサーのi2cアドレス
+    _p : multiprocessing.Process
+        センサーの値を取得してメンバを更新していく並列プロセス
     """
+
     @abstractmethod
     def __init__(self, bus, address):
+        """
+        センサ情報を登録してから、セットアップとデータ更新プロセスを開始する
+
+        Parameters
+        ----------
+        _bus : smbus2.SMBus
+            i2cのバス
+        _address : int
+            そのセンサーのi2cアドレス
+        _p : multiprocessing.Process
+            センサーの値を取得してメンバを更新していく並列プロセス
+        """
         self._bus = bus
         self._address = address
         self.setup()
@@ -19,17 +41,39 @@ class I2CSensorBase(metaclass=ABCMeta):
 
     @abstractmethod
     def setup(self):
+        """
+        接続前のモード設定などをする
+        """
         pass
 
     @abstractmethod
     def process(self):
+        """
+        センサーの値を取得してメンバを更新するプロセス
+        """
         # TODO: Lockを使うようにする
         pass
 
     @property
     def status_dict(self):
+        """
+        センサの値を表すメンバを辞書として返す。外部からは`センサーインスタンス.status_dict`のように、メンバとして呼び出せる
+
+        Returns
+        -------
+        status_dict : dict[str, int or str]
+            メンバの値の辞書
+        """
 
         def restore(data):
+            """
+            Synchronized型のデータをPythonで直接扱える形式に復元する
+
+            Parameters
+            ----------
+            data : multiprocessing.sharedctypes.Synchronized or other(default python type)
+                復元したいデータ
+            """
             if isinstance(data, Synchronized):
                 return data.value
             return data
