ab2ba4e2c70894a1b656a93ef3f923f05282e276
nanigasi_san <nanigasi.py@gmail.com>
2020-03-27 04:42:08 +0900

    Refact sensor (#22)
    * クラスの再設計・再実装
    
    * adjust arg
    
    * fix typo
    
    * write docstring
    
    * サブクラスの場所を合わせた

--- libs/sensor_mp.py
+++ libs/sensor_mp.py
@@ -1,4 +1,3 @@
-# XXX: 変換前のデータの配列を返す__read_datas関数とそれを変換する__convert_[element]関数を実装する
 from abc import ABC, abstractmethod
 from bh1792glc.driver import BH1792GLCDriver
 from ctypes import c_bool
@@ -9,38 +8,23 @@ from smbus2 import SMBus
 from time import sleep, time
 
 
-class I2CSensorBase(ABC):
+class SensorBase(ABC):
     """
-    I2Cセンサーを表す抽象基底クラス
+    センサーを表す抽象基底クラス
 
     Attributes
     ----------
-    _bus : smbus2.SMBus
-        i2cのバス
-    _address : int
-        センサーのi2cアドレス
     _is_active : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
         センサが値を更新しているか(問題なく動いているか)
     _p : multiprocessing.Process
         センサーの値を取得してメンバを更新していく並列プロセス
-
-    Notes
-    -----
-    このクラスのサブクラスとしてセンサーを表すクラスを定義する場合、@abstractmethodがついているメソッドのみをオーバーライドすればよいです
     """
 
     @abstractmethod
-    def __init__(self, *, address=None):
+    def __init__(self):
         """
         センサ情報を登録してから、セットアップとデータ更新プロセスを開始する
-
-        Parameters
-        ----------
-        _address : int
-            センサーのi2cアドレス
         """
-        self._bus = SMBus(1)
-        self._address = address
         self._is_active = Value(c_bool, True)
         try:
             self._setup()
@@ -51,6 +35,13 @@ class I2CSensorBase(ABC):
         else:
             self._p = Process(target=self._process, args=())
             self._p.start()
+
+    @abstractmethod
+    def _close(self):
+        """
+        センサー自体を閉じるメソッド。i2cのバスを閉じて、プロセスの実行も止める
+        """
+        pass
 
     @abstractmethod
     def _setup(self):
@@ -84,19 +75,6 @@ class I2CSensorBase(ABC):
             print(type(e), e)
         finally:
             self._close()
-
-    def _close(self):
-        """
-        センサー自体を閉じるメソッド。i2cのバスを閉じて、プロセスの実行も止める
-        """
-        if isinstance(self._bus, SMBus):
-            self._bus.close()
-        try:
-            if isinstance(self._ser, Serial):
-                self._ser.close()
-        except AttributeError:
-            pass
-        self._is_active.value = False
 
     @property
     def status_dict(self):
@@ -142,70 +120,49 @@ class I2CSensorBase(ABC):
         return self._is_active.value
 
 
-class Thermistor(I2CSensorBase):
+class I2CSensorBase(SensorBase):
     """
-    サーミスターを表すクラス
-
-    Attributes
-    ----------
-    type : str
-        センサーの種類(サーミスター)
-    model_number : str
-        センサーの型番
-    measured_time : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
-        現在保持しているデータを取得した時間
-    temperature_celsius : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
-        摂氏温度[℃]
-    _bus : smbus2.SMBus
-        i2cのバス
-    _address : int
-        センサーのi2cアドレス
-    _is_active : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
-        センサが値を更新しているか(問題なく動いているか)
-    _p : multiprocessing.Process
-        センサーの値を取得してメンバを更新していく並列プロセス
+    I2Cセンサー
     """
-
-    def __init__(self, address=None, signal=None, lock=None):
+    def __init__(self, address):
         """
-        センサ情報を登録してから、セットアップとデータ更新プロセスを開始する
-
         Parameters
         ----------
-        _address : int
-            センサーのi2cアドレス
+        address : int
+            I2Cスレーブのアドレス
         """
-        self._ser = Serial("/dev/ttyACM0", 9600)
-        self._lock = lock
-        self._signal = signal
-        self.type = "thermistor"
-        self.model_number = "103JT-050"
-        self.measured_time = Value("d", 0.0)
-        self.temperature_celsius = Value("d", 0.0)
+        self._bus = SMBus(1)
+        self._address = address
         super().__init__()
 
-    def _setup(self):
-        self._lock.acquire()
-        sleep(1)
-        self._ser.reset_input_buffer()
-        self._ser.reset_output_buffer()
-        self._lock.release()
+    def _close(self):
+        if isinstance(self._bus, SMBus):
+            self._bus.close()
+        self._is_active.value = False
 
-    def _update(self):
-        temp = self.__read_datas()
 
-        self.measured_time.value = time()
-        self.temperature_celsius.value = self.__convert_temperature(temp)
+class SerialSensorBase(SensorBase):
+    """
+    シリアルセンサー
+    """
+    def __init__(self, signal, lock):
+        """
+        Parameters
+        ----------
+        signal : str
+            シリアル通信で送るシグナル
+        lock : multiprocessing.Lock
+            メモリ保護のためのロック機構
+        """
+        self._ser = Serial("/dev/ttyACM0", 9600)
+        self._signal = signal
+        self._lock = lock
+        super().__init__()
 
-    def __read_datas(self):
-        self._lock.acquire()
-        self._ser.write(bytes(self._signal, "utf-8"))
-        datas = self._ser.readline()
-        self._lock.release()
-        return datas.decode("utf-8").rstrip()
-
-    def __convert_temperature(self, data):
-        return float(data)
+    def _close(self):
+        if isinstance(self._ser, Serial):
+            self._ser.close()
+        self._is_active.value = False
 
 
 class PressureSensor(I2CSensorBase):
@@ -251,7 +208,7 @@ class PressureSensor(I2CSensorBase):
         self.pressure_hpa = Value("d", 0.0)
         self.temperature_celsius = Value("d", 0.0)
         self.altitude_meters = Value("d", 0.0)
-        super().__init__(address=address)
+        super().__init__(address)
 
     def _setup(self):
         self._bus.write_byte_data(self._address, 0x20, 0xC0)  # 25Hz
@@ -277,80 +234,6 @@ class PressureSensor(I2CSensorBase):
     def __convert_altitude(self, press, temp):
         altimeter_setting_mbar = 1013.25
         return ((pow(press / altimeter_setting_mbar, 0.190263) - 1) * temp) / 0.0065
-
-
-class Accelerometer(I2CSensorBase):
-    """
-    加速度センサーを表すクラス
-
-    Attributes
-    ----------
-    type : str
-        センサーの種類(加速度センサー)
-    model_number : str
-        センサーの型番
-    measured_time : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
-        現在保持しているデータを取得した時間
-    accelerometer_x_mps2 : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
-        x軸方向の加速度
-    accelerometer_y_mps2 : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
-        y軸方向の加速度
-    accelerometer_z_mps2 : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
-        z軸方向の加速度
-    _bus : smbus2.SMBus
-        i2cのバス
-    _address : int
-        センサーのi2cアドレス
-    _is_active : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
-        センサが値を更新しているか(問題なく動いているか)
-    _p : multiprocessing.Process
-        センサーの値を取得してメンバを更新していく並列プロセス
-    """
-
-    def __init__(self, *, signal=None, lock=None):
-        """
-        センサ情報を登録してから、セットアップとデータ更新プロセスを開始する
-
-        Parameters
-        ----------
-        _address : int
-            センサーのi2cアドレス
-        """
-        self._signal = signal
-        self._lock = lock
-        self._ser = Serial("/dev/ttyACM0", 9600)
-        self.type = "accelerometer"
-        self.model_number = "KX224-1053"
-        self.measured_time = Value("d", 0.0)
-        self.accelerometer_x_mps2 = Value("d", 0.0)
-        self.accelerometer_y_mps2 = Value("d", 0.0)
-        self.accelerometer_z_mps2 = Value("d", 0.0)
-        super().__init__(address=None)
-
-    def _setup(self):
-        self._lock.acquire()
-        sleep(1)
-        self._ser.reset_input_buffer()
-        self._ser.reset_output_buffer()
-        self._lock.release()
-
-    def _update(self):
-        x, y, z = self.__read_datas()
-
-        self.measured_time.value = time()
-        self.accelerometer_x_mps2.value = self.__convert_acceleration(x)
-        self.accelerometer_y_mps2.value = self.__convert_acceleration(y)
-        self.accelerometer_z_mps2.value = self.__convert_acceleration(z)
-
-    def __read_datas(self):
-        self._lock.acquire()
-        self._ser.write(bytes(self._signal, "utf-8"))
-        datas = self._ser.readline()
-        self._lock.release()
-        return datas.decode("utf-8").rstrip().split(",")
-
-    def __convert_acceleration(self, data):
-        return float(data)
 
 
 class TemperatureHumiditySensor(I2CSensorBase):
@@ -393,7 +276,7 @@ class TemperatureHumiditySensor(I2CSensorBase):
         self.measured_time = Value("d", 0.0)
         self.temperature_celsius = Value("d", 0.0)
         self.humidity_percent = Value("d", 0.0)
-        super().__init__(address=address)
+        super().__init__(address)
 
     def _setup(self):
         self._bus.write_byte_data(self._address, 0x21, 0x30)
@@ -457,7 +340,7 @@ class PulseWaveSensor(I2CSensorBase):
         self.model_number = "BH1792GLC"
         self.measured_time = Value("d", 0.0)
         self.heart_bpm_fifo_1204hz = Value("d", 0.0)
-        super().__init__(address=address)
+        super().__init__(address)
 
     def _setup(self):
         self._bus.close()
@@ -478,12 +361,154 @@ class PulseWaveSensor(I2CSensorBase):
         return float(data[0])
 
 
+class Thermistor(SerialSensorBase):
+    """
+    サーミスターを表すクラス
+
+    Attributes
+    ----------
+    type : str
+        センサーの種類(サーミスター)
+    model_number : str
+        センサーの型番
+    measured_time : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
+        現在保持しているデータを取得した時間
+    temperature_celsius : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
+        摂氏温度[℃]
+    _ser : serial.Serial
+        シリアルポート
+    _signal : str
+        シリアルで送る文字。一桁の数字
+    _lock : multiprocessing.Lock
+        Lockインスタンス
+    _is_active : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
+        センサが値を更新しているか(問題なく動いているか)
+    _p : multiprocessing.Process
+        センサーの値を取得してメンバを更新していく並列プロセス
+    """
+
+    def __init__(self, signal, lock):
+        """
+        センサ情報を登録してから、セットアップとデータ更新プロセスを開始する
+
+        Parameters
+        ----------
+        signal : str
+            シリアル通信で送るシグナル
+        lock : multiprocessing.Lock
+            メモリ保護のためのロック機構
+        """
+        self.type = "thermistor"
+        self.model_number = "103JT-050"
+        self.measured_time = Value("d", 0.0)
+        self.temperature_celsius = Value("d", 0.0)
+        super().__init__(signal, lock)
+
+    def _setup(self):
+        self._lock.acquire()
+        sleep(1)
+        self._ser.reset_input_buffer()
+        self._ser.reset_output_buffer()
+        self._lock.release()
+
+    def _update(self):
+        temp = self.__read_datas()
+
+        self.measured_time.value = time()
+        self.temperature_celsius.value = self.__convert_temperature(temp)
+
+    def __read_datas(self):
+        self._lock.acquire()
+        self._ser.write(bytes(self._signal, "utf-8"))
+        datas = self._ser.readline()
+        self._lock.release()
+        return datas.decode("utf-8").rstrip()
+
+    def __convert_temperature(self, data):
+        return float(data)
+
+
+class Accelerometer(SerialSensorBase):
+    """
+    加速度センサーを表すクラス
+
+    Attributes
+    ----------
+    type : str
+        センサーの種類(加速度センサー)
+    model_number : str
+        センサーの型番
+    measured_time : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
+        現在保持しているデータを取得した時間
+    accelerometer_x_mps2 : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
+        x軸方向の加速度
+    accelerometer_y_mps2 : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
+        y軸方向の加速度
+    accelerometer_z_mps2 : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
+        z軸方向の加速度
+    _ser : serial.Serial
+        シリアルポート
+    _signal : str
+        シリアルで送る文字。一桁の数字
+    _lock : multiprocessing.Lock
+        Lockインスタンス
+    _is_active : multiprocessing.sharedctypes.Synchronized(ctypes.c_bool)
+        センサが値を更新しているか(問題なく動いているか)
+    _p : multiprocessing.Process
+        センサーの値を取得してメンバを更新していく並列プロセス
+    """
+
+    def __init__(self, signal, lock):
+        """
+        センサ情報を登録してから、セットアップとデータ更新プロセスを開始する
+
+        Parameters
+        ----------
+        signal : str
+            シリアル通信で送るシグナル
+        lock : multiprocessing.Lock
+            メモリ保護のためのロック機構
+        """
+        self.type = "accelerometer"
+        self.model_number = "KX224-1053"
+        self.measured_time = Value("d", 0.0)
+        self.accelerometer_x_mps2 = Value("d", 0.0)
+        self.accelerometer_y_mps2 = Value("d", 0.0)
+        self.accelerometer_z_mps2 = Value("d", 0.0)
+        super().__init__(signal, lock)
+
+    def _setup(self):
+        self._lock.acquire()
+        sleep(1)
+        self._ser.reset_input_buffer()
+        self._ser.reset_output_buffer()
+        self._lock.release()
+
+    def _update(self):
+        x, y, z = self.__read_datas()
+
+        self.measured_time.value = time()
+        self.accelerometer_x_mps2.value = self.__convert_acceleration(x)
+        self.accelerometer_y_mps2.value = self.__convert_acceleration(y)
+        self.accelerometer_z_mps2.value = self.__convert_acceleration(z)
+
+    def __read_datas(self):
+        self._lock.acquire()
+        self._ser.write(bytes(self._signal, "utf-8"))
+        datas = self._ser.readline()
+        self._lock.release()
+        return datas.decode("utf-8").rstrip().split(",")
+
+    def __convert_acceleration(self, data):
+        return float(data)
+
+
 if __name__ == "__main__":
     lock = Lock()
-    Th1 = Thermistor(signal="1", lock=lock)
-    Th2 = Thermistor(signal="2", lock=lock)
+    Th1 = Thermistor("1", lock)
+    Th2 = Thermistor("2", lock)
     Pr = PressureSensor()
-    Ac = Accelerometer(signal="5", lock=lock)
+    Ac = Accelerometer("5", lock)
     THs = TemperatureHumiditySensor()
     Pw = PulseWaveSensor()
     while True:
