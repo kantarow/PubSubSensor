0dcce0121e6ed5a26998924d88d2fb0f41801883
nanigasi_san <nanigasi.py@gmail.com>
2020-03-28 18:49:06 +0900

    lint

--- examples/paho_subscribe_example.py
+++ examples/paho_subscribe_example.py
@@ -1,15 +1,20 @@
 import paho.mqtt.client as mqtt
 
 # クライアントがサーバーからCONNACKを受けとった際に呼ばれるコールバック
+
+
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
 
     # on_connect()にsubscribe()を記述すると, 接続が切断されても再び購読できる
     client.subscribe("example/topic")
 
 # クライアントがサーバーからPUBLISHを受け取った際に呼ばれるコールバック
+
+
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.payload))
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
